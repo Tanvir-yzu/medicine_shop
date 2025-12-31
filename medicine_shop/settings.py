caec0ef84658aef6e2e2e3b08e8e764cbb5de435
Tanvir Islam <2020tanvir1971@gmail.com>
2025-12-31 21:15:26 +0800

    build: add jazzmin and configure static/media files
    Add jazzmin to INSTALLED_APPS for admin interface customization
    Configure static files directories and media URL/root settings

--- medicine_shop/settings.py
+++ medicine_shop/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'jazzmin',
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
@@ -114,8 +115,16 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
+# Static files (CSS, JavaScript, Images)
+STATIC_URL = '/static/'
 
-STATIC_URL = "static/"
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
+
 
 # medicine_shop/settings.py
 APPEND_SLASH = True
